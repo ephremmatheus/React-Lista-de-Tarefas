3646e922f03f92081921ef7bec541bce2666a5c7
ephremmatheus <ephrem08matheus@gmail.com>
2025-02-27 12:30:03 -0300

    correção de caminho

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route  } from "react-router-dom";
 import App from './App.jsx'
-import TaskPage from './pages/taskPage.jsx';
+import TaskPage from './pages/TaskPage.jsx';
 import './index.css'
 
 
